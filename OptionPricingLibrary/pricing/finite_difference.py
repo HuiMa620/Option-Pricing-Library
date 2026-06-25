400f8325a17270510636f78934f7e0e6c871dc4c
HuiMa620 <huima620@gmail.com>
2026-06-24 21:09:59 -0400

    Add files via upload

--- OptionPricingLibrary/pricing/finite_difference.py
+++ OptionPricingLibrary/pricing/finite_difference.py
@@ -127,6 +127,77 @@ class ExplicitFiniteDifferenceEngine():
         
         
         
+class ImplicitFiniteDifferenceEngine:
+    def __init__(self, s_max = 300.0, n_s= 200, n_t = 500):
+        self.s_max = s_max
+        self.n_s = n_s
+        self.n_t = n_t
+        
+        
+    def price(self, option, market):
+        S0 = option.spot
+        K = option.strike
+        T = option.tau
+        option_type = option.option_type
+        
+        r = market.rate
+        y = market.dividend
+        sigma = market.volatility
+        
+        dS = self.s_max / self.n_s
+        dt = T / self.n_t
+        
+        S_grid = np.linspace(0.0, self.s_max, self.n_s + 1)
+        
+        #boundary condition at tau = 0
+        if option_type == 'Call':
+            V = np.maximum(S_grid - K, 0)
+        elif option_type == 'Put':
+            V = np.maximum(K - S_grid, 0)
+        else:
+            raise ValueError("option_type must be 'Call' or 'Put'.")
+            
+        #vectorize
+        i = np.arange(1, self.n_s)
+        S_inner = S_grid[i]
+        
+        A = 0.5*sigma**2 * S_inner**2
+        B = (r - y)*S_inner
+        
+        lower = - dt*(A/dS **2 - B /(2*dS))
+        diag = 1.0 + dt*(2*A/dS**2 + r)
+        upper = -dt*(A/dS**2 + B/(2*dS))
+        
+        #tridiagonal matrix
+        M = np.zeros((self.n_s - 1, self.n_s - 1))
+        np.fill_diagonal(M, diag)
+        np.fill_diagonal(M[1:], lower[1:])
+        np.fill_diagonal(M[:,1:], upper[:-1])
+        
+        for step in range(self.n_t):
+            tau = (step + 1) * dt
+            
+            #boundary condition at the to sides of S
+            if option_type == 'Call':
+                leftB = 0.0
+                rightB = self.s_max * np.exp(-y*tau) - K*np.exp(-r*tau)
+            else:
+                leftB = K*np.exp(-r*tau)
+                rightB = 0.0
+                
+            rhs = V[1:-1].copy()
+            rhs[0] -= lower[0] * leftB
+            rhs[-1] -= upper[-1] * rightB
+            
+            V_inner_new = np.linalg.solve(M, rhs)
+            
+            V[0] = leftB
+            V[-1] = rightB
+            V[1:-1] = V_inner_new
+            
+        price = np.interp(S0, S_grid, V)
+        return price
+                
         
         
         
@@ -137,10 +208,3 @@ class ExplicitFiniteDifferenceEngine():
         
         
         
-        
-        
-        
-        
-        
-        
-
