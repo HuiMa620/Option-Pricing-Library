014cf01eb9742d2a8923c9f52ea165e4c97be252
HuiMa620 <huima620@gmail.com>
2026-06-25 22:54:02 -0400

    Add files via upload

---
+++ OptionPricingLibrary/tests/test_american_put_implicit_finite_difference.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 22:35:11 2026
+
+@author: ma6
+"""
+
+import os
+os.chdir('..')
+
+from pricing.products import EuropeanOption
+from pricing.market import MarketData
+from pricing.finite_difference import (
+    ImplicitFiniteDifferenceEngine,
+    AmericanPutImplicitFiniteDifferenceEngine
+    )
+
+def test_american_put_greater_than_european_put():
+    put = EuropeanOption(
+        spot = 100,
+        strike = 110,
+        tau = 0.5,
+        option_type = 'Put'
+        )
+    
+    market = MarketData(
+        rate = 0.04,
+        dividend = 0.02,
+        volatility = 0.25
+        )
+    
+    european_engine = ImplicitFiniteDifferenceEngine(
+        s_max = 300,
+        n_s = 200,
+        n_t = 500
+        )
+    
+    american_engine = AmericanPutImplicitFiniteDifferenceEngine(
+        s_max = 300,
+        n_s = 200,
+        n_t = 500
+        )
+    
+    european_put_price = european_engine.price(put, market)
+    american_put_price = american_engine.price(put, market)
+    
+    assert american_put_price >= european_put_price
+    
+
+def test_american_put_price_above_intrinsic_value():
+    put = EuropeanOption(
+        spot = 100,
+        strike = 110,
+        tau = 0.5,
+        option_type = 'Put'
+        )
+    
+    market = MarketData(
+        rate = 0.04,
+        dividend = 0.02,
+        volatility = 0.25
+        )
+    
+    american_engine = AmericanPutImplicitFiniteDifferenceEngine(
+        s_max = 300,
+        n_s = 200,
+        n_t = 500
+        )
+    
+    american_put_price = american_engine.price(put, market)
+    intrinsic_value = max(put.strike - put.spot, 0.0)
+    
+    assert american_put_price >= intrinsic_value
+    
+
+
+
+
+
+
+
+
---
+++ OptionPricingLibrary/tests/test_crank_nicolson_finite_difference.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 13:16:40 2026
+
+@author: ma6
+"""
+
+import os
+os.chdir('..')
+
+from pricing.products import EuropeanOption
+from pricing.market import MarketData
+from pricing.black_scholes import BlackScholesEngine
+from pricing.finite_difference import CrankNicolsonFiniteDifferenceEngine
+
+def test_crank_nicolson_vs_black_scholes_call():
+    call = EuropeanOption(
+        spot = 100,
+        strike = 110,
+        tau = 0.5,
+        option_type = 'Call'
+        )
+    
+    market = MarketData(
+        rate = 0.04,
+        dividend = 0.02,
+        volatility = 0.25
+        )
+    
+    bs_engine = BlackScholesEngine()
+    bs_price = bs_engine.price(call, market)
+    
+    cn_engine = CrankNicolsonFiniteDifferenceEngine()
+    cn_price = cn_engine.price(call, market)
+    
+    assert abs(cn_price - bs_price) < 0.05
+
+
+def test_crank_nicolson_vs_black_scholes_put():
+    put = EuropeanOption(
+        spot = 100,
+        strike = 110,
+        tau = 0.5,
+        option_type = 'Put'
+        )
+    
+    market = MarketData(
+        rate = 0.04,
+        dividend = 0.02,
+        volatility = 0.25
+        )
+    
+    bs_engine = BlackScholesEngine()
+    bs_price = bs_engine.price(put, market)
+    
+    cn_engine = CrankNicolsonFiniteDifferenceEngine()
+    cn_price = cn_engine.price(put, market)
+    
+    assert abs(cn_price - bs_price) < 0.05
+
+
+
+
+
+
+
+
+
+
+
