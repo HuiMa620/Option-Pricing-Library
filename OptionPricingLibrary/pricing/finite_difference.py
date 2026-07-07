e1bf5b857369b6654baa43e77ca98990cec06728
HuiMa620 <huima620@gmail.com>
2026-07-06 21:43:59 -0400

    Add files via upload

--- OptionPricingLibrary/pricing/finite_difference.py
+++ OptionPricingLibrary/pricing/finite_difference.py
@@ -16,7 +16,7 @@ class ExplicitFiniteDifferenceEngine():
         self.n_s= n_s
         self.n_t = n_t
         
-    def price_loop(self, option, market):
+    def price_loop(self, option, market):# slow version based on for loop
         S0 = option.spot
         K = option.strike
         T = option.tau
@@ -85,6 +85,16 @@ class ExplicitFiniteDifferenceEngine():
         
         dS = self.s_max / self.n_s
         dt = T / self.n_t
+        
+        max_i = self.n_s
+        stability_ratio = dt*sigma**2 * max_i**2
+        
+        if stability_ratio > 0.5:
+            raise ValueError(
+                "Explicit finite difference scheme may be unstable."
+                "Increase n_t or decrease n_s."
+                f"Current dt*sigma**2 * n_s**2 = {stability_ratio:.10f}"
+                )
         
         S_grid = np.linspace(0.0, self.s_max, self.n_s + 1)
         
@@ -185,10 +195,10 @@ class ImplicitFiniteDifferenceEngine:
                 leftB = K*np.exp(-r*tau)
                 rightB = 0.0
                 
-            rhs = V[1:-1].copy()
-            rhs[0] -= lower[0] * leftB
-            rhs[-1] -= upper[-1] * rightB
-            
+            rhs = V[1:-1].copy()             #The matrix did not include V0 and V_n_s. The boudanry here need to be corrected
+            rhs[0] -= lower[0] * leftB       #Otherwise, the first row is b_1 * V_1 + c_1 * V_2 = V_1_old
+            rhs[-1] -= upper[-1] * rightB    #But it supposed to be a_1 * V_0 + b_1 * V_1 + c_1 * V_2 = V_1_old
+                                             #The rhs is corrected as V_1_old - a_1*V_0. The same for V_n_1
             V_inner_new = np.linalg.solve(M, rhs)
             
             V[0] = leftB
@@ -199,7 +209,6 @@ class ImplicitFiniteDifferenceEngine:
         return price
                 
         
-
         
 
 class CrankNicolsonFiniteDifferenceEngine():
@@ -353,7 +362,7 @@ class AmericanPutImplicitFiniteDifferenceEngine():
         exercise_inner = np.maximum(K - S_inner, 0.0)
         
         for step in range(self.n_t):
-            tau = (step + 1)*dt
+            #tau = (step + 1)*dt
             
             #boundary condition at two sides of S
             leftB = K
@@ -379,6 +388,100 @@ class AmericanPutImplicitFiniteDifferenceEngine():
         return price
             
             
+        
+        
+        
+class AmericanPutCrankNicolsonFiniteDifferenceEngine():
+    
+    def __init__(self, s_max = 300, n_s = 200, n_t = 500):
+        self.s_max = s_max
+        self.n_s = n_s
+        self.n_t = n_t
+        
+    def price(self, option, market):
+        S0 = option.spot
+        K = option.strike
+        T = option.tau
+        option_type = option.option_type
+        
+        r = market.rate
+        y = market.dividend
+        sigma = market.volatility
+        
+        dS = self.s_max / self.n_s
+        dt = T / self.n_t
+        S_grid = np.linspace(0.0, self.s_max, self.n_s + 1)
+        
+        #boundary condition at tau = 0
+        if option_type == 'Put':
+            V = np.maximum(K - S_grid, 0)
+        else:
+            raise ValueError("option_type must be 'Put'.")
+            
+        #vectorize S
+        i = np.arange(1, self.n_s)
+        S_inner = S_grid[i]
+        
+        A = 0.5*sigma**2 * S_inner**2
+        B = (r - y)*S_inner
+        
+        #operator L coefficients
+        L_lower = A / dS**2 - B / (2.0 * dS)
+        L_diag = -2.0 * A / dS**2 - r
+        L_upper = A / dS**2 + B / (2.0 * dS)
+        
+        #left hand side: I + 0.5*dt*L
+        lhs_lower = -0.5*dt*L_lower
+        lhs_diag = 1.0 - 0.5*dt*L_diag
+        lhs_upper = -0.5 *dt*L_upper
+        
+        #righr hand side: I + 0.5* dt*L
+        rhs_lower = 0.5*dt*L_lower
+        rhs_diag = 1.0 + 0.5*dt*L_diag
+        rhs_upper = 0.5*dt*L_upper
+        
+        #left hand side matrix
+        M_left = np.zeros((self.n_s-1, self.n_s-1))
+        np.fill_diagonal(M_left, lhs_diag)
+        np.fill_diagonal(M_left[1:], lhs_lower[1:])
+        np.fill_diagonal(M_left[:,1:], lhs_upper[:-1])
+        
+        #rigth hand side matrix
+        M_right = np.zeros((self.n_s - 1, self.n_s - 1))
+        np.fill_diagonal(M_right, rhs_diag)
+        np.fill_diagonal(M_right[1:], rhs_lower[1:])
+        np.fill_diagonal(M_right[:,1:], rhs_upper[:-1])
+        
+        exercise_inner = np.maximum(K - S_inner, 0.0)
+        for step in range(self.n_t):
+            #tau_old = step*dt
+            #tau_new = (step + 1)*dt
+            
+            #old boundary condition
+            left_old = K
+            right_old = 0.0
+            
+            left_new = K
+            right_new = 0.0
+                
+            #rhs from old interior values
+            rhs = M_right @ V[1:-1]
+            
+            rhs[0] += rhs_lower[0] * left_old
+            rhs[-1] += rhs_upper[-1] * right_old
+            
+            rhs[0] -= lhs_lower[0]*left_new
+            rhs[-1] -= lhs_upper[-1]*right_new
+            
+            V_continuation_inner = np.linalg.solve(M_left, rhs)
+            V_inner_new = np.maximum(exercise_inner, V_continuation_inner)
+            
+            V[0] = left_new
+            V[-1] = right_new
+            V[1:-1] = V_inner_new
+            
+        price = np.interp(S0, S_grid, V)
+        return price
         
         
         
