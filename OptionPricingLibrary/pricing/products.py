e8a4fb781325241871c7b0f0bb04cfb24e418d5f
HuiMa620 <huima620@gmail.com>
2026-07-23 17:30:53 -0400

    Implement input validation for option classes
    Added input validation for EuropeanOption and BarrierOption classes.

--- OptionPricingLibrary/pricing/products.py
+++ OptionPricingLibrary/pricing/products.py
@@ -16,6 +16,58 @@ class EuropeanOption:
     strike: float
     tau: float
     option_type: str # 'Call' or 'Put'
+    
+    def __post_init__(self):
+        self._validate_inputs()
+        
+    def _validate_inputs(self):
+        if self.spot <= 0:
+            raise ValueError('spot must be positive.')
+        
+        if self.strike <= 0:
+            raise ValueError('strike must be positive.')
+            
+        if self.tau <= 0:
+            raise ValueError('tau must be positive.')
+            
+        if self.option_type not in ['Call','Put']:
+            raise ValueError("option_type must be 'Call' or 'Put'.")
+            
+
+@dataclass
+class BarrierOption:
+    spot: float
+    strike: float
+    barrier: float        # barrier level
+    tau: float
+    option_type: str      # Call or Put
+    barrier_type: str     # Down or Up
+    knock_type: str       # Out first version only support Out
+    
+    def __post_init__(self):
+        self._validate_inputs()
+    
+    def _validate_inputs(self):
+        if self.spot <= 0:
+            raise ValueError('spot must be positive.')
+        
+        if self.strike <= 0:
+            raise ValueError('strike must be positive.')
+        
+        if self.barrier <= 0:
+            raise ValueError('barrier must be positive.')
+        
+        if self.tau <= 0:
+            raise ValueError('tau must be positive.')
+            
+        if self.option_type not in ['Call','Put']:
+            raise ValueError("option_type must be 'Call' or 'Put'.")
+            
+        if self.barrier_type not in ['Down', 'Up']:
+            raise ValueError("barrier_type must be 'Down' or 'Up'.")
+        
+        if self.knock_type != 'Out':
+            raise ValueError("Only knock_type = 'Out' is currently supported.")
 
 
 
